ae48cbf0578a8e3746b3c3ed0867463cf403a10c
Nohe <nmesfien@gmail.com>
2023-04-19 11:27:10 -0700

    strings and integers

---
+++ Basic_C#_Program/Strings_and_Integers/Strings_and_Integers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_and_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> number = new List<int>{360, 50, 1500 };
+                Console.WriteLine(String.Join(", ", number));
+                Console.WriteLine("Choose number to divide these numbers by:");
+                int divide = Convert.ToInt32(Console.ReadLine());
+                for (int x = 0; x < number.Count; x++)
+                {
+                    int answer = number[x] / divide;
+                    string j = Convert.ToString(x);
+                    Console.WriteLine(number[x] + "/" + divide + " = " + answer);
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("try again");
+            }
+           
+        }
+    }
+}
