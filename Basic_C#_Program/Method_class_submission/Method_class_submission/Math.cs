aded60f0d39b0164f1804844bf7754ee73e20f9a
Nohe <nmesfien@gmail.com>
2023-04-24 12:23:39 -0700

    method_class

---
+++ Basic_C#_Program/Method_class_submission/Method_class_submission/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Method_class_submission
+{
+    public class Math
+    {
+        public void multiply(int input, int input2)
+
+        {
+        
+            int answer = input * 2;
+            Console.WriteLine( "the second integer is " +input2);
+            Console.WriteLine(input+ " * " + 2 + " = " + answer);
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Basic_C#_Program/Method_class_submission/Method_class_submission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Method_class_submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Math Math2 = new Math();
+
+          
+             Math2.multiply(3, 4);
+
+        }
+    }
+}
