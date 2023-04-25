af72c5437e0893487ada05751c217129ff7150af
Nohe <nmesfien@gmail.com>
2023-04-25 11:37:53 -0700

    class submission

--- Basic_C#_Program/Class_submission/Class_submission/Math.cs
+++ Basic_C#_Program/Class_submission/Class_submission/Math.cs
@@ -3,13 +3,18 @@ namespace Class_submission
 {
     public class Math
     {
-        public void Mathmethod(int input, int input2)
+        public void Mathmethod(int input, int input2=2)
         {
-        
-            int answer = input / 2;
-            Console.WriteLine(answer);
-        }
 
-      
+            int answer = input / input2;
+            Console.WriteLine(input + "/" + "2" + "=" + answer);
+        }
+        public static int Mathmethod(out int x, out int y)
+        {
+
+            x = 5;
+            y = 20;
+            return y / x;
+        }
     }
 }
--- Basic_C#_Program/Class_submission/Class_submission/Program.cs
+++ Basic_C#_Program/Class_submission/Class_submission/Program.cs
@@ -4,13 +4,22 @@ namespace Class_submission
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+
+
             Math Math2 = new Math();
 
-
+            Console.WriteLine("Enter number to divide by two:");
             int userinput = Convert.ToInt32(Console.ReadLine());
-            Math2.Mathmethod(userinput, 4);
+            Math2.Mathmethod(userinput,2);
+
+
+            //static class
+            Console.WriteLine("this is the static output\n"+"20/5="+Math.Mathmethod(out int x, out int y));
+
         }
+
     }
 }
