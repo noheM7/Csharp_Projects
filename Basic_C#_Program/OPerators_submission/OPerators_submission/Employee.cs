46db154f82dff31af9a84f4d7c09f1d7f5db5631
Nohe <nmesfien@gmail.com>
2023-04-29 22:27:19 -0700

    operators

--- Basic_C#_Program/OPerators_submission/OPerators_submission/Employee.cs
+++ Basic_C#_Program/OPerators_submission/OPerators_submission/Employee.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-namespace OPerators_submission
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Operators_submission
 {
-    public class Employee
+    public class Employee 
     {
-
         public string Id
-        { get;set; }
+        { get; set; }
 
         public string FirstName
         { get; set; }
@@ -13,5 +16,38 @@ namespace OPerators_submission
         public string LastName
         { get; set; }
 
-    } 
+
+
+
+
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.Id == employee2.Id) ;
+
+
+                return true;
+            
+
+
+
+        }
+
+
+
+
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee.Id != employee2.Id;
+        }
+
+       
+        
+
     }
+
+    
+}
+
+
+       
--- Basic_C#_Program/OPerators_submission/OPerators_submission/Program.cs
+++ Basic_C#_Program/OPerators_submission/OPerators_submission/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Operators_submission;
 
 namespace OPerators_submission
 {
@@ -6,7 +7,20 @@ namespace OPerators_submission
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee person1 = new Employee();
+
+            person1.FirstName = "Connor";
+            person1.LastName = "McGregor";
+            person1.Id = "1";
+
+            Employee person2 = new Employee();
+            person2.FirstName = "Michael";
+            person2.LastName = "Jordan";
+            person2.Id = "12";
+
+            Console.WriteLine("T/F does employee 1 have the same id as employee 2? "); 
+            Console.WriteLine(person1.Id == person2.Id); 
+            Console.ReadLine();
         }
     }
 }
