3acbdba3cf8b3ab806512033d1aeb451b5085849
Nohe <nmesfien@gmail.com>
2023-05-02 16:39:51 -0700

    constructors

---
+++ Basic_C#_Program/Constructor/Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int x = 8;
+            var z = 9;
+
+            chain chain2 = new chain();
+            Console.WriteLine(x+ " is the constant.\n"+ z + " is the variable.\n" + chain2.Name + chain2.Id + " is the constructors together.");
+        }
+    }
+}
---
+++ Basic_C#_Program/Constructor/Constructor/chain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Constructor
+{
+    public class chain
+    {
+       public string Name
+        { get; set; }
+
+        public int Id
+        { get; set; }
+
+        public chain( int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+
+
+        public chain() : this(14567, "Jason ")
+        {
+
+        }
+
+    }
+    
+}
