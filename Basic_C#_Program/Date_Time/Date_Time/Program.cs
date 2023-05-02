a0e888bb39e8d571df75410ea7103da69f5c72e6
Nohe <nmesfien@gmail.com>
2023-05-02 12:34:00 -0700

    datetime

---
+++ Basic_C#_Program/Date_Time/Date_Time/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            DateTime now = DateTime.Now;
+            Console.WriteLine("input a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            DateTime usertime = now.AddHours(x);
+            Console.WriteLine("it will be "+ usertime+ " in "+ x+ " hours");
+           
+
+        }
+    }
+}
