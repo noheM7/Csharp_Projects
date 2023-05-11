cee6f2230b16c6bdb46184023e580e1b81486d42
noheM7 <nmesfien@gmail.com>
2023-05-11 15:26:20 -0700

    update

---
+++ Basic_C#_Program/StudentDatabases/StudentDatabases/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Reflection.Metadata;
+
+namespace StudentDatabases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                // Create and save a new Blog
+                Console.Write("Enter Students name: ");
+                var name = Console.ReadLine();
+
+                var blog = new Student { Name = name };
+                db.Students.Add(blog);
+                db.SaveChanges();
+                var query = from b in db.Students
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        } 
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string Name { get; set;}
+        }
+        public class StudentContext : DbContext
+        {
+            public DbSet<Student> Students { get; set; }
+        }
+    }
+}
